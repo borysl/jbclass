e377443f99a48325a6464ec5b5fd9b38e1261d50
borys <borys.lebeda@gmail.com>
2011-09-21 11:39:20 +0300

    Refactor test
    Make all 3 test + 1 for Null reference

--- PointsOfSale/Sales.Tests/SimpleSalesTest.cs
+++ PointsOfSale/Sales.Tests/SimpleSalesTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using SimpleScanner;
 
 namespace Sales.Tests
@@ -6,16 +7,46 @@ namespace Sales.Tests
     [TestFixture]
     public class SimpleSalesTest
     {
+        private SalesPoint _salesPoint;
+        private Scanner _scanner;
+        private Screen _screen;
+
+        [SetUp]
+        public void InitializeScanningSystem()
+        {
+            _salesPoint = new SalesPoint();
+            _scanner = new Scanner();
+            _screen = new Screen();
+            _salesPoint.PlugScanner(_scanner);
+            _salesPoint.PlugLcdScreen(_screen);
+        }
+
         [Test]
         public void ArticleShouldGetPriceInEuros()
         {
-            var salesPoint = new SalesPoint();
-            var scanner = new Scanner();
-            var screen = new Screen();
-            salesPoint.PlugScanner(scanner);
-            salesPoint.PlugLcdScreen(screen);
-            salesPoint.Scan("12345");
-            Assert.AreEqual("EUR 500.00", screen.Display);
+            _salesPoint.Scan("12345");
+            Assert.AreEqual("EUR 500.00", _screen.Display);
+        }
+
+        [Test]
+        public void EmptyBarcodeOutputsError()
+        {
+            _salesPoint.Scan(string.Empty);
+            Assert.AreEqual("Scan error: Empty barcode", _screen.Display);
+        }
+
+        [Test]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void NullBarcodeProducesException()
+        {
+            _salesPoint.Scan(null);
+        }
+
+        [Test]
+        public void NotProductFoundOutputsError()
+        {
+            _salesPoint.Scan("999999");
+            Assert.AreEqual("No product found: 999999", _screen.Display);
         }
     }
 }
