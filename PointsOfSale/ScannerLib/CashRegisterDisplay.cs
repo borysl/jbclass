85d908b60b20b4d1a23b1fa61e251b26c9fa2a54
borys <borys.lebeda@gmail.com>
2011-09-22 15:27:54 +0300

    Move class to separate file

--- PointsOfSale/ScannerLib/CashRegisterDisplay.cs
+++ PointsOfSale/ScannerLib/CashRegisterDisplay.cs
@@ -48,17 +48,4 @@ namespace ScannerLib
                 testPrice.PstIncluded ? "P" : string.Empty);
         }
     }
-
-    public class ProductPriceInfo : PriceWithTaxes
-    {
-        public double Cost
-        {
-            get
-            {
-                var fullPrice = NetPrice * 1.05;
-                if (PstIncluded) fullPrice *= 1.1;
-                return fullPrice;
-            }
-        }
-    }
 }
---
+++ PointsOfSale/ScannerLib/ProductPriceInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace ScannerLib
+{
+    public class ProductPriceInfo : PriceWithTaxes
+    {
+        public double Cost
+        {
+            get
+            {
+                var fullPrice = NetPrice * 1.05;
+                if (PstIncluded) fullPrice *= 1.1;
+                return fullPrice;
+            }
+        }
+    }
+}
